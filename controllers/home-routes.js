5f49bad820c1efb3e46ffbc940be310991ce5096
Rodrigo <rodvalenciaa@gmail.com>
2022-04-27 14:41:13 -0700

    add login to destroy session

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,7 @@ const { Post, User, Comment } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
+  console.log(req.session);
   console.log('======================');
   Post.findAll({
     attributes: [
@@ -40,6 +41,10 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
   res.render('login');
 });
 
