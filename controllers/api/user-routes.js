0446f20333755ffffdd673729d1f8b4d13eebab0
Rodrigo <rodvalenciaa@gmail.com>
2022-04-28 16:19:18 -0700

    add helper functions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -59,19 +59,20 @@ router.post('/', (req, res) => {
     username: req.body.username,
     email: req.body.email,
     password: req.body.password
-  }) 
-  .then(dbUserData => {
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-  
-      res.json(dbUserData);
+  })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  }).catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
 });
 
 router.post('/login', (req, res) => {
@@ -94,7 +95,6 @@ router.post('/login', (req, res) => {
     }
 
     req.session.save(() => {
-      // declare session variables
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
@@ -104,7 +104,6 @@ router.post('/login', (req, res) => {
   });
 });
 
-// Logout route
 router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
@@ -113,7 +112,7 @@ router.post('/logout', (req, res) => {
   } else {
     res.status(404).end();
   }
-})
+});
 
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
@@ -126,7 +125,7 @@ router.put('/:id', (req, res) => {
     }
   })
     .then(dbUserData => {
-      if (!dbUserData[0]) {
+      if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
